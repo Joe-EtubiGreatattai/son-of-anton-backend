5ccccd573d340b2d7c5e4b64b0dbf01e6469af9b
Joe-EtubiGreatattai <greatattai442442@gmail.com>
2025-12-23 13:45:57 +0100

    refactor: remove deprecated Mongoose connection options

--- server.js
+++ server.js
@@ -134,10 +134,7 @@ function validateSearchFrequency() {
 validateSearchFrequency();
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
 

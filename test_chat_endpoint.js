e29c4acd4e86e3ef1618288d9911b21fcb67ea32
Joe-EtubiGreatattai <greatattai442442@gmail.com>
2026-01-17 15:27:10 +0100

    test: Update backend URLs in test scripts to point to the deployed service.

--- test_chat_endpoint.js
+++ test_chat_endpoint.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const API_URL = 'http://192.168.0.127:3000/api/chat';
+const API_URL = 'https://son-of-anton-backend-1n0j.onrender.com/api/chat';
 
 async function testChatEndpoint() {
     console.log('Testing /api/chat endpoint...\n');
